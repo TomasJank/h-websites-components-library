192a6f16e924c331bd79529c4e59174dd8961659
Tomas Jankauskas <tomas@Tomass-MacBook-Pro.local>
2022-01-07 19:46:59 +0900

    initial commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<!-- <script>
+import Vue from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { HWebsitesComponentsLibrarySample } from '@/entry.esm';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  // components: {
+  //  HWebsitesComponentsLibrarySample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <h-websites-components-library-sample />
+  </div>
+</template> -->
